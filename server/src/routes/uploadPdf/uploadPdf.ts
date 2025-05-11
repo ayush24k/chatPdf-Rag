7d70fa28f149a2a99ade14bbcd60a6ef36523737
ayush24k <ayushanshu69@gmail.com>
2025-05-11 17:43:20 +0530

    queue system integration

--- server/src/routes/uploadPdf/uploadPdf.ts
+++ server/src/routes/uploadPdf/uploadPdf.ts
@@ -1,6 +1,16 @@
 import express from 'express'
 import multer from 'multer';
+import { Queue } from 'bullmq';
 
+const queue = new Queue('file-upload-queue', {
+    connection: {
+        host: 'localhost',
+        port: 6379
+    }
+});
+
+
+// multer file handling 
 const fileStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/')
@@ -17,5 +27,10 @@ const upload = multer({ storage: fileStorage })
 export const uploadRouter = express.Router();
 
 uploadRouter.post('/pdf', upload.single('pdf'), (req, res) => {
+    queue.add('file-ready', JSON.stringify({
+        filename: req.file?.originalname,
+        source: req.file?.destination,
+        path: req.file?.path,
+    }))
     res.json({ message: "uploaded" });
 })
